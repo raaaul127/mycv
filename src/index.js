ed6f5ec8c70e05119c26ca42317e664a392e91a4
Frontend JavaScript <frontendjavascript@iMAC-09.local>
2025-03-12 20:50:47 +0200

    mycv

---
+++ src/index.js
@@ -0,0 +1,11 @@
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
+// Import our custom CSS
+import './scss/styles.scss'
+
+// Import https://github.com/mattboldt/typed.js/
+// import Typed from 'typed.js';
+import '../node_modules/typed.js/dist/typed.umd.js'
+
+
+import'./js/main.js'
